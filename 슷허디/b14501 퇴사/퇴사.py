34fec057ac51f4e8cb9d0a63d1b8ecb39c9b6e5e
joyhee <jhee0046@naver.com>
2024-09-02 23:48:50 +0900

    귀가학습

--- 슷허디/b14501 퇴사/퇴사.py
+++ 슷허디/b14501 퇴사/퇴사.py
@@ -1,3 +1,22 @@
-N = int(input())
-li = [list(map(int, input().split())) for _ in range(N)]
+import sys
+sys.stdin = open('input.txt', 'r')
 
+T = int(input())
+for i in range(T):
+    N = int(input())
+    li = [list(map(int, input().split())) for _ in range(N)]
+    # dp = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+
+    # li[i][0]을 잘 골라서
+    # li[i][1]을 최대로
+    dp = [0] * (N+1)
+
+    for j in range(N):
+        s, p = li[j][0], li[j][1]
+        if N < N-j-s:
+            continue
+        for i in range(j, j+s+1):
+            if dp[i] < dp[s+i]+p:
+                dp[i] = dp[s+i]+p
+
+    print(f"#{max(dp)}")
